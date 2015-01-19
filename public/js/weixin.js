9a3f3608c0e00b34a118a48ad339889660954bb3
zzy <zzy@holidaycloud.cn>
2015-01-19 14:25:20 +0800

    weixin jsapi test

--- public/js/weixin.js
+++ public/js/weixin.js
@@ -11,6 +11,14 @@ wx.ready(function () {
             alert(JSON.stringify(res));
         }
     });
+    wx.scanQRCode({
+        desc: 'scanQRCode desc',
+        needResult: 1, // 默认为0，扫描结果由微信处理，1则直接返回扫描结果，
+        scanType: ["qrCode","barCode"], // 可以指定扫二维码还是一维码，默认二者都有
+        success: function (res) {
+            alert(JSON.stringify(res));
+        }
+    });
 });
 
 wx.error(function (res) {
