a507cc5339334867a0a7457b949f2b394feae86c
zzy <zzy@holidaycloud.cn>
2015-01-19 15:26:56 +0800

    weixin jsapi test

--- public/js/my-app.js
+++ public/js/my-app.js
@@ -13,7 +13,6 @@ var mainView = myApp.addView('.view-main', {
         // Because we want to use dynamic navbar, we need to enable it for this view:
         dynamicNavbar: true
 });
-mainView.router.loadPage("detail?p=54a36ddbd62713a35a8e727e");
 wx.ready(function () {
         $$('#scanner').on('click', function (e) {
                 wx.scanQRCode({
@@ -21,10 +20,11 @@ wx.ready(function () {
                         needResult: 1, // 默认为0，扫描结果由微信处理，1则直接返回扫描结果，
                         scanType: ["qrCode","barCode"], // 可以指定扫二维码还是一维码，默认二者都有
                         success: function (res) {
-                                alert(JSON.stringify(res));
-                                var scanResult = res.resultStr.scan_code.scan_result;
+                                alert(res.resultStr);
+                                var result = JSON.parse(res.resultStr);
+                                var scanResult = result.scan_code.scan_result;
                                 alert(scanResult);
-                                mainView.router.loadPage("detail?p="+scanResult.split(",")[1]);
+                                mainView.router.loadPage("detail?p="+scanResult);
                         }
                 });
         });
