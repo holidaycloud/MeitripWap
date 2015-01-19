fdfad63559722f277861558fc4604593a6872ec6
zzy <zzy@holidaycloud.cn>
2015-01-19 14:56:54 +0800

    weixin jsapi test

--- public/js/my-app.js
+++ public/js/my-app.js
@@ -16,7 +16,7 @@ var mainView = myApp.addView('.view-main', {
 wx.ready(function () {
         $$('#scanner').on('click', function (e) {
                 wx.scanQRCode({
-                        desc: 'scanQRCode desc',
+                        desc: '扫一扫',
                         needResult: 1, // 默认为0，扫描结果由微信处理，1则直接返回扫描结果，
                         scanType: ["qrCode","barCode"], // 可以指定扫二维码还是一维码，默认二者都有
                         success: function (res) {
