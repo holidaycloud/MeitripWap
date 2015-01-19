5a379211b0d9a060dbb8be0867a746c6a70c23fa
zzy <zzy@holidaycloud.cn>
2015-01-19 16:00:08 +0800

    weixin jsapi test

--- public/js/my-app.js
+++ public/js/my-app.js
@@ -15,32 +15,31 @@ var mainView = myApp.addView('.view-main', {
 });
 wx.ready(function () {
         wx.hideOptionMenu();
-        wx.onMenuShareTimeline({
-                title: 'test', // 分享标题
-                link: 'http://test.meitrip.net/#!/detail?p=54a36ddbd62713a35a8e727e', // 分享链接
-                imgUrl: 'http://holidaycloud.b0.upaiyun.com/582d29f881ea54667bc7a4a5d5b7b782.jpg', // 分享图标
-                success: function () {
-                        // 用户确认分享后执行的回调函数
-                        console.log('success');
-                },
-                cancel: function () {
-                        // 用户取消分享后执行的回调函数
-                        console.log('cancel');
-                }
-        });
+
         $$('#scanner').on('click', function (e) {
-                wx.scanQRCode({
-                        desc: '扫一扫',
-                        needResult: 1, // 默认为0，扫描结果由微信处理，1则直接返回扫描结果，
-                        scanType: ["qrCode","barCode"], // 可以指定扫二维码还是一维码，默认二者都有
-                        success: function (res) {
-                                alert(res.resultStr);
-                                var result = JSON.parse(res.resultStr);
-                                var scanResult = result.scan_code.scan_result;
-                                alert(scanResult);
-                                mainView.router.loadPage("detail?p="+scanResult);
+                wx.onMenuShareTimeline({
+                        title: 'test', // 分享标题
+                        link: 'http://test.meitrip.net/#!/detail?p=54a36ddbd62713a35a8e727e', // 分享链接
+                        imgUrl: 'http://holidaycloud.b0.upaiyun.com/582d29f881ea54667bc7a4a5d5b7b782.jpg', // 分享图标
+                        success: function () {
+                                // 用户确认分享后执行的回调函数
+                                console.log('success');
+                        },
+                        cancel: function () {
+                                // 用户取消分享后执行的回调函数
+                                console.log('cancel');
                         }
                 });
+                //wx.scanQRCode({
+                //        desc: '扫一扫',
+                //        needResult: 1, // 默认为0，扫描结果由微信处理，1则直接返回扫描结果，
+                //        scanType: ["qrCode","barCode"], // 可以指定扫二维码还是一维码，默认二者都有
+                //        success: function (res) {
+                //                var result = JSON.parse(res.resultStr);
+                //                var scanResult = result.scan_code.scan_result;
+                //                mainView.router.loadPage("detail?p="+scanResult);
+                //        }
+                //});
         });
 });
 
