c1d3f14e63136f65244c1be1511209995e95567f
zzy <zzy@holidaycloud.cn>
2015-01-19 15:18:35 +0800

    weixin jsapi test

--- public/js/my-app.js
+++ public/js/my-app.js
@@ -20,6 +20,7 @@ wx.ready(function () {
                         needResult: 1, // 默认为0，扫描结果由微信处理，1则直接返回扫描结果，
                         scanType: ["qrCode","barCode"], // 可以指定扫二维码还是一维码，默认二者都有
                         success: function (res) {
+                                alert(JSON.stringify(res));
                                 var scanResult = res.resultStr.scan_code.scan_result;
                                 alert(scanResult);
                                 mainView.router.loadPage("/product?id="+scanResult.split(",")[1]);
