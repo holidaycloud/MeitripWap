ab28cedb2bb02f8143810e0213f630c8822461ab
zzy <zzy@holidaycloud.cn>
2015-01-19 14:09:53 +0800

    weixin jsapi test

--- public/js/weixin.js
+++ public/js/weixin.js
@@ -1,3 +1,18 @@
 /**
  * Created by zzy on 2015/1/19.
  */
+wx.ready(function () {
+    wx.checkJsApi({
+        jsApiList: [
+            'getNetworkType',
+            'previewImage'
+        ],
+        success: function (res) {
+            alert(JSON.stringify(res));
+        }
+    });
+});
+
+wx.error(function (res) {
+    alert(res.errMsg);
+});
